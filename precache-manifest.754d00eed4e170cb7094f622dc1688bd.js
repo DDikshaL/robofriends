33632454cedfa35d287683888c1fdcff180401f9
Diksha Dedhiya <dikshadedhiya@gmail.com>
2022-06-16 15:45:10 +0530

    Updates

---
+++ precache-manifest.754d00eed4e170cb7094f622dc1688bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ed4b1cf5376a87b6944",
+    "url": "/robofriends/static/css/main.43e57bdc.chunk.css"
+  },
+  {
+    "revision": "8ed4b1cf5376a87b6944",
+    "url": "/robofriends/static/js/main.8ed4b1cf.chunk.js"
+  },
+  {
+    "revision": "6793f3988c77f137d2b9",
+    "url": "/robofriends/static/css/1.670518a9.chunk.css"
+  },
+  {
+    "revision": "6793f3988c77f137d2b9",
+    "url": "/robofriends/static/js/1.6793f398.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "29ff360af8bf87f20920faa3405d2bf3",
+    "url": "/robofriends/index.html"
+  }
+];
